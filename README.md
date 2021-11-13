109b38c2e6c6ddb909be733899b7ac846ebe64a2
Amir Ahmadi <eamahma@gmail.com>
2021-11-13 12:08:22 -0500

    sample carousel in react

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+** Source:
+https://www.youtube.com/watch?v=iyj3TZXg2gQ
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/Helper/CarouselData.js
@@ -0,0 +1,9 @@
+import Rooster from '../images/rooster.jpg';
+import Wings from '../images/wings.jpg';
+import Boat from '../images/boat.jpg';
+
+export const images = [
+    { title: "Rooster", subtitle: "This is beautifull Rooster", alt: "Rooster", img: Rooster},
+    { title: "Wings", subtitle: "Colorfull wings", alt: "Wings", img: Wings},
+    { title: "Boat", subtitle: "Boat in sunset", alt: "Boat", img: Boat},
+];
